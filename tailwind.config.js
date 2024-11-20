65fdf59b5833388cfeb0350c6ab2737787237ccc
Kamoliddinmirzaboyev05 <kamoliddinmirzaboyev2005@gmail.com>
2024-11-20 10:16:11 +0500

    Update

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        michroma: ['Michroma', 'sans-serif'], // Fontni nomlash
+      },
+    },
+  },
+  plugins: [],
+}
